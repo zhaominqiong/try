69d81e145f4e22396fd047968c457d5ca758ffc7
zhaominqiong <1302354606@qq.com>
2019-03-15 15:54:16 +0800

    update A.py

--- A.py
+++ A.py
@@ -1,5 +1,3 @@
 C:\Users\Administrator\PycharmProjects\untitled4\BCReval.py -i /home/server3/data2/zmq/ENCFF585HYM.fastq
 
 C:\Users\Administrator\PycharmProjects\untitled4\BCReval.py -i /home/server3/data2/zmq/ENCFF585HYM.fastq
-
-C:\Users\Administrator\PycharmProjects\untitled4\BCReval.py -i /home/server3/data2/zmq/ENCFF585HYM.fastq
